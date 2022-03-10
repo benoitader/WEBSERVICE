e7764835adead0e20ab312cb116b7e818c0df6b4
Benoit Ader <benoitader.dev@gmail.com>
2022-03-10 18:29:13 +0100

    Ajouts des 'headers' requis

--- produits/lire.php
+++ produits/lire.php
@@ -1,1 +1,8 @@
 <?php
+
+// headers requis (necessaire pour les controls et autorisations)
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8"); // Contenu de la réponse
+header("Access-Control-Allow-Methods: GET"); // Methodes utilistées pour la requête (en Rest on utilise GET pour lire)
+header("Access-Control-Max-Age: 3600"); // Durée de vie de la requête
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"); // Les headers autorisés vis à vis du POST client
