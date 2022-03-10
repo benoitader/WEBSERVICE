0cf28596b60501d9be38fdc109643678a314aa5b
Benoit Ader <benoitader.dev@gmail.com>
2022-03-10 20:18:09 +0100

    Gestion d'erreur de méthode

--- produits/lire.php
+++ produits/lire.php
@@ -6,3 +6,13 @@ header("Content-Type: application/json; charset=UTF-8"); // Contenu de la répon
 header("Access-Control-Allow-Methods: GET"); // Methodes utilistées pour la requête (en Rest on utilise GET pour lire)
 header("Access-Control-Max-Age: 3600"); // Durée de vie de la requête
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"); // Les headers autorisés vis à vis du POST client
+
+
+// On vérifie que la méthode utilisée est correcte
+if($_SERVER['REQUEST_METHOD'] = 'GET'){
+  
+}else{
+  // On gère l'erreur
+  http_reponse_code(405);
+  echo json_encode(["message" => "La méthode n'est pas autorisée"]);
+}
