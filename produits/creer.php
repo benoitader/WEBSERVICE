ef78d0b396b6dee17a05038db06a539f37bf6802
Benoit Ader <benoitader.dev@gmail.com>
2022-03-11 11:25:35 +0100

    Initialisation de 'Créer'

--- produits/creer.php
+++ produits/creer.php
@@ -1,1 +1,16 @@
 <?php
+// headers requis (necessaire pour les controls et autorisations)
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8"); // Contenu de la réponse
+header("Access-Control-Allow-Methods: POST"); // Methodes utilistées pour la requête (en Rest on utilise GET pour lire)
+header("Access-Control-Max-Age: 3600"); // Durée de vie de la requête
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"); // Les headers autorisés vis à vis du POST client
+
+// On vérifie la méthode
+if($_SERVER['REQUEST_METHOD'] = 'POST'){
+
+}else{
+  // On gère l'erreur
+  http_reponse_code(405);
+  echo json_encode(["message" => "La méthode n'est pas autorisée"]);
+}
